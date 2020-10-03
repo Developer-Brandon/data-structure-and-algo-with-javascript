cc70b22e95d11827c631d358496e9ac40a64735b
brandonLee <lkc2252@gmail.com>
2020-10-03 16:19:42 +0900

    - Quick sort finished

--- 14.quick-sort/14.js
+++ 14.quick-sort/14.js
@@ -1,5 +1,29 @@
 function quickSort(list) {
- let leftSideList = []
- let rightSideList = []
-
+ if (list.length <= 1) {
+  return list
+ } else {
+  let standardValue = list[0]
+  let leftSideList = []
+  let rightSideList = []
+  for (let i = 1; i < list.length; i++) {
+   if (standardValue > list[i]) {
+    leftSideList.push(list[i])
+   } else {
+    rightSideList.push(list[i])
+   }
+  }
+  return quickSort(leftSideList).concat(standardValue, quickSort(rightSideList))
+ }
 }
+
+let randomList = []
+
+for (let i = 0; i < 5; i++) {
+ let myRandom = Math.floor(Math.random() * (100 + 1));
+ randomList.push(myRandom);
+}
+
+console.log('\n')
+console.log('Random list: ', randomList)
+console.log('\n')
+console.log('After quick sort: ', quickSort(randomList))
